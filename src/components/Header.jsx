01dddc6c3073ddfd4453a13d9f5007bdc1cd2440
Erik <93394265+Erik-TH@users.noreply.github.com>
2023-01-30 19:01:19 +0100

    component Header : sign out

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,53 @@
-import { NavLink } from "react-router-dom"
-import argentBankLogo from "../assets/argentBankLogo.png"
+import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+import argentBankLogo from "../assets/argentBankLogo.png";
 
 export default function Header() {
-    return (
-        <header className="component header">
-            <nav className="main-nav">
-                <NavLink to="/" className="main-nav-logo">
-                    <img src={argentBankLogo} alt="ArgentBank's logo" className="main-nav-logo-image" />
-                    <h1 className="sr-only">Argent Bank</h1>
-                </NavLink>
+  const isAuthenticatedUser = useSelector((state) => state.isAuthenticatedUser);
 
-                <div>
-                    <NavLink to="/login" className="main-nav-item">
-                        <i className="fa fa-user-circle"></i>
-                        Sign In
-                    </NavLink>
-                </div>
-            </nav>
-        </header>
-    )
+  const dispatch = useDispatch();
+
+  return (
+    <header className="component header">
+      <nav className="main-nav">
+        <NavLink to="/" className="main-nav-logo">
+          <img
+            src={argentBankLogo}
+            alt="ArgentBank's logo"
+            className="main-nav-logo-image"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
+        </NavLink>
+
+        {isAuthenticatedUser ? (
+          <div>
+            <NavLink to="/profile" className="main-nav-item">
+              <i className="fa fa-user-circle"></i>
+              Name
+            </NavLink>
+
+            <NavLink
+              to="/"
+              className="main-nav-item"
+              onClick={(event) => {
+                event.preventDefault();
+                dispatch({ type: "loginUser" });
+              }}
+            >
+              <i className="fa fa-sign-out"></i>
+              Sign Out
+            </NavLink>
+          </div>
+        ) : (
+          <div>
+            <NavLink to="/login" className="main-nav-item">
+              <i className="fa fa-user-circle"></i>
+              Sign In
+            </NavLink>
+          </div>
+        )}
+      </nav>
+    </header>
+  );
 }
