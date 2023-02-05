ee94cf31c79db84d3d81fd69f0186da6135654cd
Erik <93394265+Erik-TH@users.noreply.github.com>
2023-02-06 00:10:58 +0100

    Profile : update edit name

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -48,7 +48,7 @@ export default function Profile() {
     })
     .catch((error) => dispatch(profileError(error.response.data.message)));
 
-  const [editBtn, setEditBtn] = useState("");
+  const [isEditing, setIsEditing] = useState("");
   const [userFullName, setUserFullName] = useState({
     firstName: "",
     lastName: "",
@@ -64,7 +64,7 @@ export default function Profile() {
 
   function editUserBtn(e) {
     e.preventDefault();
-    setEditBtn((current) => !current);
+    setIsEditing((current) => !current);
   }
 
   async function submitHandler(e) {
@@ -75,67 +75,19 @@ export default function Profile() {
       const newUser = await userUpdateName(userFullName);
       dispatch(profileFirstName(newUser.body.firstName));
       dispatch(profileLastName(newUser.body.lastName));
-      setEditBtn((current) => !current);
+      setIsEditing((current) => !current);
     } catch (error) {
       dispatch(profileError(error.response.data.message));
     }
   }
 
   const content = (
-    <>
-      {!editBtn ? (
-        <main className="main bg-dark">
-          <div className="header">
-            <h1>
-              Welcome back
-              <br />
-              {firstName + " " + lastName} !
-            </h1>
-            <button className="edit-button" onClick={editUserBtn}>
-              Edit Name
-            </button>
-          </div>
-          <h2 className="sr-only">Accounts</h2>
-          <section className="account">
-            <div className="account-content-wrapper">
-              <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-              <p className="account-amount">$2,082.79</p>
-              <p className="account-amount-description">Available Balance</p>
-            </div>
-            <div className="account-content-wrapper cta">
-              <button className="transaction-button">View transactions</button>
-            </div>
-          </section>
-          <section className="account">
-            <div className="account-content-wrapper">
-              <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-              <p className="account-amount">$10,928.42</p>
-              <p className="account-amount-description">Available Balance</p>
-            </div>
-            <div className="account-content-wrapper cta">
-              <button className="transaction-button">View transactions</button>
-            </div>
-          </section>
-          <section className="account">
-            <div className="account-content-wrapper">
-              <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-              <p className="account-amount">$184.30</p>
-              <p className="account-amount-description">Current Balance</p>
-            </div>
-            <div className="account-content-wrapper cta">
-              <button className="transaction-button">View transactions</button>
-            </div>
-          </section>
-        </main>
-      ) : (
-        <main className="main bg-dark">
-          <div className="header">
-            <h1>
-              Welcome back
-              <br />
-              {firstName + " " + lastName} !
-            </h1>
-
+    <main className="main bg-dark">
+      <div className="header">
+        <h1>
+          Welcome back
+          <br />
+          {isEditing ? (
             <form className="editNameForm" onSubmit={submitHandler}>
               <div>
                 <input
@@ -146,7 +98,9 @@ export default function Profile() {
                   onChange={handleChange}
                   required
                 />
-
+                <button className="edit-button" type="submit">
+                  Save
+                </button>
               </div>
 
               <div>
@@ -158,51 +112,53 @@ export default function Profile() {
                   onChange={handleChange}
                   required
                 />
-
-              </div>
-                <button className="edit-button" type="submit">
-                  Save
-                </button>
                 <button className="edit-button" onClick={editUserBtn}>
                   Cancel
                 </button>
+              </div>
             </form>
-          </div>
-
-          <h2 className="sr-only">Accounts</h2>
-          <section className="account">
-            <div className="account-content-wrapper">
-              <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-              <p className="account-amount">$2,082.79</p>
-              <p className="account-amount-description">Available Balance</p>
-            </div>
-            <div className="account-content-wrapper cta">
-              <button className="transaction-button">View transactions</button>
-            </div>
-          </section>
-          <section className="account">
-            <div className="account-content-wrapper">
-              <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-              <p className="account-amount">$10,928.42</p>
-              <p className="account-amount-description">Available Balance</p>
-            </div>
-            <div className="account-content-wrapper cta">
-              <button className="transaction-button">View transactions</button>
-            </div>
-          </section>
-          <section className="account">
-            <div className="account-content-wrapper">
-              <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-              <p className="account-amount">$184.30</p>
-              <p className="account-amount-description">Current Balance</p>
-            </div>
-            <div className="account-content-wrapper cta">
-              <button className="transaction-button">View transactions</button>
-            </div>
-          </section>
-        </main>
-      )}
-    </>
+          ) : (
+            <div>{firstName + " " + lastName} !</div>
+          )}
+        </h1>
+        {!isEditing && (
+          <button className="edit-button" onClick={editUserBtn}>
+            Edit Name
+          </button>
+        )}
+      </div>
+      <h2 className="sr-only">Accounts</h2>
+      <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+          <p className="account-amount">$2,082.79</p>
+          <p className="account-amount-description">Available Balance</p>
+        </div>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </section>
+      <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
+          <p className="account-amount">$10,928.42</p>
+          <p className="account-amount-description">Available Balance</p>
+        </div>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </section>
+      <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+          <p className="account-amount">$184.30</p>
+          <p className="account-amount-description">Current Balance</p>
+        </div>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </section>
+    </main>
   );
 
   return content;
